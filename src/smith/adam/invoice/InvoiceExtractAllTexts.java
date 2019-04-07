4aa10711d099db058b32551a03536d3406febe2d
Adam <ratedsmith256@gmail.com>
2019-04-07 18:04:46 -0500

    Current working version. The program now will generate a Excel sheet based on inputed data based on the Java program

--- src/smith/adam/invoice/InvoiceExtractAllTexts.java
+++ src/smith/adam/invoice/InvoiceExtractAllTexts.java
@@ -2,7 +2,10 @@ package smith.adam.invoice;
 
 import javafx.scene.control.TextField;
 
+import java.io.File;
+import java.io.PrintStream;
 import java.util.List;
+
 
 public class InvoiceExtractAllTexts {
 
@@ -14,19 +17,66 @@ public class InvoiceExtractAllTexts {
         List<String> descriptions = additionalCosts.getDescriptions();
         List<Double> prices = additionalCosts.getPrices();
 
-        System.out.println("Customer Info \n");
+        String currentDirectory = System.getProperty("user.dir");
+        String pathToFile = currentDirectory +"\\ExcelFiles\\DataFiles\\DataFile.txt";
 
-        for(TextField cust : customerInfo){
-            System.out.println(cust.getText());
+        String command = "python "+ currentDirectory+"\\ExcelFiles\\capstone_script\\venv\\capstone.py";
+        System.out.println(command);
+        File myFile = new File(pathToFile);
+
+        try{
+            PrintStream writeToFile = new PrintStream(myFile);
+
+            System.out.println("Writing Customer Info \n");
+            int x = 0;
+            for(TextField cust : customerInfo){
+                if(x == 3){
+                    writeToFile.println("End");
+                    if(cust.getText().equals("")){
+                        System.out.println(cust.getText());
+                        writeToFile.println("NA");
+                        x++;
+                    }else {
+                        System.out.println(cust.getText());
+                        writeToFile.println(cust.getText());
+                        x++;
+                    }
+                }else if(x == 5) {
+                    writeToFile.println("End");
+                    x++;
+                }else
+                 {
+                    System.out.println(cust.getText());
+                    writeToFile.println(cust.getText());
+                    x++;
+                }
+
+            }
+            writeToFile.println("End");
+
+            System.out.println("\nAll additional Costs that may have been entered\n");
+
+            for(int i = 0; i < descriptions.size(); i++){
+                writeToFile.println(String.format("%s, %.2f,", descriptions.get(i), prices.get(i)));
+
+                System.out.println(String.format("Description = %s , price = %.2f", descriptions.get(i), prices.get(i)));
+            }
+            writeToFile.println("End");
+            System.out.println(String.format("\nBid price is %.2f", Double.parseDouble(bidPrice.getText())));
+            writeToFile.println(String.format("%.2f", Double.parseDouble(bidPrice.getText())));
+            writeToFile.println("End");
+
+            writeToFile.close();
+
+            Runtime.getRuntime().exec(command);
+
+
+        }catch(Exception C){
+            System.out.println("Error writing to file" + C);
         }
 
-        System.out.println("\nAll additional Costs that may have been entered\n");
 
-        for(int i = 0; i < descriptions.size(); i++){
-            System.out.println(String.format("Description = %s , price = %.2f", descriptions.get(i), prices.get(i)));
-        }
 
-        System.out.println(String.format("\nBid price is %.2f", Double.parseDouble(bidPrice.getText())));
     }
 
 }
